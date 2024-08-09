488dfe0cbd661a93ba332368eaa20d7506af064d
Alina Hagan <alina-hagan@outlook.com>
2024-08-09 16:14:55 +0100

    Listener trait implementation, prints data to stdout

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,31 @@ use tokio::sync::Notify;
 
 use lightstreamer_client::ls_client::{LightstreamerClient, Transport };
 use lightstreamer_client::subscription::{Subscription, SubscriptionMode, Snapshot};
+use lightstreamer_client::subscription_listener::SubscriptionListener;
+use lightstreamer_client::item_update::ItemUpdate;
+
+
+
+struct ISSListener{}
+
+impl SubscriptionListener for ISSListener {
+    fn on_item_update(&self, update: &ItemUpdate) {
+
+        let not_available = "N/A".to_string();
+        let item_name = update.item_name.clone().unwrap_or(not_available.clone());
+        let fields = vec!["TimeStamp", "Value" ];
+
+        let mut output = String::new();
+
+        for field in fields {
+            let value = update.get_value(field).unwrap_or(&not_available).clone();
+            output.push_str(&format!("{}: {}, ", field, value.to_string()));
+        }
+        println!("{}, {}", item_name, output);
+    }
+
+}
+
 
 #[tokio::main]
 async fn main() {
@@ -30,7 +55,10 @@ async fn main() {
         //Some( Snapshot::Yes );
     )
     .unwrap();
+    //subscription.set_data_adapter( Some(String::from("ISSLIVE")) ).unwrap();
     subscription.set_requested_snapshot( Some(Snapshot::Yes ) ).unwrap();
+    subscription.add_listener( Box::new(ISSListener{} ));
+    
 
     client.subscribe( subscription );
     let notify = Arc::new(Notify::new());
